e07207b815b433f292c11c1bed76273123ec984d
OmarFateh <66747309+OmarFateh@users.noreply.github.com>
2021-05-18 23:00:15 +0200

    update user pic

--- project/settings/production.py
+++ project/settings/production.py
@@ -20,11 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'h$)g()(@+a&kp#xd3eoy2quq*tio&u%x&j@#p3*7580cb+pe68'
-# SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -73,7 +72,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     # custom
-    # 'accounts.middlewares.RestrictionRolesMiddleWare',
+    'accounts.middlewares.RestrictionRolesMiddleWare',
 ]
 
 LOGIN_URL = 'login/'
@@ -111,8 +110,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'd81d35rf94s1ft',
         'USER': 'lvueumkxxcvsgs',
-        'PASSWORD': 'ad106136712054acda260f6081c212db49ffec3613e93175dbe3f084836ef60f',
-        # 'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
         'HOST': 'ec2-54-72-155-238.eu-west-1.compute.amazonaws.com',
         'PORT': '5432',
     }
@@ -167,10 +165,8 @@ MEDIA_URL = '/student-management-system/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': 'dtao7b1hg',
-    'API_KEY': '159866617856165',
-    'API_SECRET': 'A04KLeInxqS1Mw_lZxlSgT__uEs',
-    # 'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
-    # 'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
 }
 
 # Authentication Backends
@@ -183,8 +179,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'fatehomar0@gmail.com'
-EMAIL_HOST_PASSWORD = 'ltpopqfqbdoweugo'
-# EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 # Unicode slugs
 ALLOW_UNICODE_SLUGS = True
